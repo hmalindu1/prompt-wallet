8a8e0d2a88336afe364cd1a252338aa0a328d5a3
hmalindu1 <hemachandra.hashan@gmail.com>
2024-02-05 17:10:37 +0530

    Home page title added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1>Save, Reuse and Share your Best Prompts</h1>
+    </section>
   )
 }
 
