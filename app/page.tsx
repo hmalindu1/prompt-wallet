d1af1f9beb82c72755d587c7280a491f2dcc22b4
hmalindu1 <hemachandra.hashan@gmail.com>
2024-02-06 09:40:43 +0530

    Homepage title and description

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
-      <h1>Save, Reuse and Share your Best Prompts</h1>
-    </section>
-  )
-}
+      <h1 className="head_text text-center">
+        Save & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> Your Best AI-Prompts</span>
+      </h1>
+      <p className="desc text-center"> Prompt Wallet is an open-source AI prompt saving tool for anyone to save and share the best prompts</p>
 
-export default Home
+      {/* <Feed /> */}
+    </section>
+  );
+};
+
+export default Home;
