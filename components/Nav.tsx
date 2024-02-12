cb6223eeb5ea57d5c98d567adc4d0f717185fd9d
hmalindu1 <hemachandra.hashan@gmail.com>
2024-02-12 10:45:12 +0530

    Nav logo text changed

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -14,7 +14,7 @@ const Nav = () => {
       setProviders(response);
     };
     setUpProviders();
-  })
+  });
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -25,7 +25,7 @@ const Nav = () => {
           height={30}
           className="object-contain"
         />
-        <p className="logo_text">Promptopia</p>
+        <p className="logo_text">Prompt Wallet</p>
       </Link>
 
       {/* Desktop Navigation */}
