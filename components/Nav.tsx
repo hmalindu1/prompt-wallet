ac01b1817c83f6bfaa5cece57408a0edec3a42ff
hmalindu1 <hemachandra.hashan@gmail.com>
2024-02-13 09:59:25 +0530

    Next Auth Providers

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -49,7 +49,17 @@ const Nav = () => {
             </Link>
           </div>
         ) : (
-          <></>
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >Sign In</button>
+              ))}
+          </>
         )}
       </div>
     </nav>
