f9bfe42439afc0714a00f7502abafff7c89a9371
hmalindu1 <hemachandra.hashan@gmail.com>
2024-02-16 16:09:35 +0530

    User session and sign in

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+import User from "@/models/user";
 import { connectToDB } from "@/utils/db";
 import nextAuth from "next-auth";
 import Google from "next-auth/providers/google";
@@ -10,18 +11,39 @@ const handler = nextAuth({
     }),
   ],
   async session({ session }) {
-      
+    const sessionUser = await User.findOne({
+      email: session.user?.email, 
+    })
+
+    session.user.id = sessionUser?._id.toString();
+
+    return session;
   },
 
   async signIn({ profile }) {
-      try {
-        await connectToDB();
+    try {
+      await connectToDB();
 
-        
-      } catch (error) {
-        
+      // check if a user already exists
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+
+      // if not, create a new user
+      if ( !userExists ) {
+        await User.create({
+          email: profile.email,
+          username: profile.name?.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
       }
-  }
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 });
 
 export { handler as GET, handler as POST };
